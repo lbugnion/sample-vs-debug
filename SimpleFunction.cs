b792ada76d5998bc4bd63303b6f0831a2c92d99a
Laurent Bugnion <lbugnion@outlook.com>
2024-01-30 18:00:17 +0100

    Upgrading function

--- SimpleFunction.cs
+++ SimpleFunction.cs
@@ -1,33 +1,32 @@
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Azure.Data.Tables;
-using System;
 using SimpleFunction.Model;
 
-namespace FunctionApp1
+namespace SimpleFunction
 {
-    public static class SimpleFunction
+    public class SimpleFunction
     {
-        [FunctionName("SimpleFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(
-                AuthorizationLevel.Function, 
-                "get", 
-                "post", 
-                Route = null)]
-            HttpRequest req,
-            [Table("visitors")]
-            TableClient tableClient,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        private readonly ILogger<SimpleFunction> _logger;
 
+        public SimpleFunction(ILogger<SimpleFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(SimpleFunction))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous, 
+                "get", 
+                "post")]
+            HttpRequest req,
+            [TableInput("visitors")]
+            TableClient tableClient)
+        {
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -36,7 +35,7 @@ namespace FunctionApp1
 
             if (string.IsNullOrEmpty(name))
             {
-                log.LogError("Name cannot be empty!");
+                _logger.LogError("Name cannot be empty!");
                 return new BadRequestObjectResult("Server error");
             }
 
@@ -52,14 +51,7 @@ namespace FunctionApp1
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            log.LogInformation("This is an information");
-            log.LogDebug($"This is debug: {entity.VisitorName}");
-            log.LogTrace("This is a trace");
-
             return new OkObjectResult(responseMessage);
-
-            //var json = JsonConvert.SerializeObject(entity);
-            //return new OkObjectResult(json);
         }
     }
 }
